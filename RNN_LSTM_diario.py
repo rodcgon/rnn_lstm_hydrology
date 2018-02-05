1b52a399ab61bb7c95749c5d0feae8f569ed24fd
Rodrigo Goncalves <rodcgon@gmail.com>
2018-02-05 18:27:22 -0200

    Update RNN_LSTM_diario.py

--- RNN_LSTM_diario.py
+++ RNN_LSTM_diario.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import time
-#import csv
 from keras.layers.core import Dense, Activation, Dropout
 from keras.layers.recurrent import LSTM
 from keras.models import Sequential
